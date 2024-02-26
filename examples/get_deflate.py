f605fa9976eff1e68e1c921831c9733d7f4b8955
Christopher Arndt <chris@chrisarndt.de>
2024-02-26 22:03:23 +0100

    feat: make zlib module optional in deflate response example
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- examples/get_deflate.py
+++ examples/get_deflate.py
@@ -1,10 +1,24 @@
 """Send GET HTTP request and request and handle 'deflate' compression of response.
 
-Requires the 'zlib' module from micropython-lib or the CPython standard library.
+Requires the 'deflate' module added in Micropython v1.21, the 'zlib' module from
+micropython-lib, or the CPython standard library.
 
 """
 
-import zlib
+try:
+    # Use MicroPython's 'deflate' module, if available,
+    # to avoid dependency on and RAM cost of 'zlib' module.
+    import deflate
+except ImportError:
+    import zlib
+
+    def decompress(resp):
+        return zlib.decompress(resp.content)
+else:
+    def decompress(resp, wbits=10):
+        with deflate.DeflateIO(resp._sf, deflate.ZLIB, wbits) as g:
+            return g.read()
+
 import mrequests
 
 
@@ -15,15 +29,17 @@ r = mrequests.get(url, headers={b"TE": b"deflate"}, save_headers=True)
 
 if r.status_code == 200:
     print("Response headers:")
+    print("-----------------\n")
     print(b"\n".join(r.headers).decode())
 
     if r.encoding.split(",")[0] == "deflate":
-        text = zlib.decompress(r.content).decode("utf-8")
+        text = decompress(r).decode("utf-8")
         print("Deflated response text length: %i" % len(text))
     else:
         text = r.text
 
     print("Response text:")
+    print("--------------\n")
     print(text)
 else:
     print("Request failed. Status: {}".format(r.status_code))
