11c77343c48cfdcd8893ac915ac1e394962ab3f3
Christopher Arndt <chris@chrisarndt.de>
2024-02-26 22:02:39 +0100

    fix: esp8266 port not supported for DNS-over-HTTPS example
    Signed-off-by: Christopher Arndt <chris@chrisarndt.de>

--- examples/dns_over_https.py
+++ examples/dns_over_https.py
@@ -34,6 +34,9 @@ def gethostbyname(name):
 if __name__ == "__main__":
     import sys
 
+    if sys.platform == "esp8266":
+        sys.exit("The MicroPython esp8266 port lacks proper TLS cipher support for this example.")
+
     name = sys.argv[1] if len(sys.argv) > 1 else "httpbin.org"
     res = gethostbyname(name)
 
